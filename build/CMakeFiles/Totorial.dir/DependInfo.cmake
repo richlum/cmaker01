88510a196c2ca0e0b92b5df8866333c3ff9fd02b
richard lum <a00470425@gmail.com>
2015-08-02 19:37:19 -0700

    Initial step 1

---
+++ build/CMakeFiles/Totorial.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/richlum/Documents/dev/cmake/src/tutorial.cxx" "/Users/richlum/Documents/dev/cmake/build/CMakeFiles/Totorial.dir/tutorial.cxx.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "."
+  "/Users/richlum/Documents/dev/cmake/src/MathFunctions"
+  )
+set(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+set(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ build/CMakeFiles/Totorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Totorial.dir/tutorial.cxx.o"
+  "Totorial.pdb"
+  "Totorial"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Totorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
